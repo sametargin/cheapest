090fcc4df72548c1764c842ada4db0afe29c38d9
Samet <samet@Samets-MacBook-Air.local>
2025-05-20 23:32:51 -0400

    🚀 Auto update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Cheapest!</h1>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +22,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export default App;
