5981b0ee0586e18eb9817c32e6f8677f33a92e53
Samet <samet@Samets-MacBook-Air.local>
2025-05-20 23:54:42 -0400

    🚀 Auto update

--- src/products.js
+++ src/products.js
@@ -1,24 +1,55 @@
 const products = [
   {
-    id: "xiaomi-vacuum",
-    name: "Xiaomi Mi Robot Vacuum Mop 2 Pro",
-    image: "https://cdn.cimri.io/image/1000x1000/xiaomi-mi-robot-vacuum.jpg",
+    id: "ps5",
+    name: "PlayStation 5 Console",
+    image: "https://m.media-amazon.com/images/I/619BkvKW35L._AC_SL1500_.jpg",
     prices: [
-      { country: "ABD", price: 399.99, currency: "USD" },
-      { country: "Almanya", price: 369.99, currency: "EUR" },
-      { country: "Türkiye", price: 13999, currency: "TRY" }
-    ]
+      { country: "USA", price: 499, currency: "USD", price_usd: 499 },
+      { country: "Germany", price: 549, currency: "EUR", price_usd: 586 },
+      { country: "Turkey", price: 21999, currency: "TRY", price_usd: 684 },
+    ],
   },
   {
-    id: "philips-tv",
-    name: "Philips 55'' 4K UHD TV",
-    image: "https://images.philips.com/is/image/PhilipsConsumer/55oled808",
+    id: "macbook",
+    name: "MacBook Air M2",
+    image: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/macbook-air-m2-hero-2022",
     prices: [
-      { country: "ABD", price: 599.99, currency: "USD" },
-      { country: "Almanya", price: 549.99, currency: "EUR" },
-      { country: "Türkiye", price: 24999, currency: "TRY" }
-    ]
-  }
+      { country: "USA", price: 1099, currency: "USD", price_usd: 1099 },
+      { country: "Germany", price: 1299, currency: "EUR", price_usd: 1387 },
+      { country: "Turkey", price: 45999, currency: "TRY", price_usd: 1431 },
+    ],
+  },
+  {
+    id: "echo",
+    name: "Amazon Echo Dot (5th Gen)",
+    image: "https://m.media-amazon.com/images/I/61u0y9ADElL._AC_SL1000_.jpg",
+    prices: [
+      { country: "USA", price: 49.99, currency: "USD", price_usd: 49.99 },
+      { country: "Germany", price: 59.99, currency: "EUR", price_usd: 63 },
+      { country: "Turkey", price: 1999, currency: "TRY", price_usd: 62 },
+    ],
+  },
+  {
+    id: "dyson",
+    name: "Dyson V15 Detect Vacuum",
+    image: "https://dyson-h.assetsadobe2.com/is/image/content/dam/dyson/images/products/primary/447930-01.png",
+    prices: [
+      { country: "USA", price: 749, currency: "USD", price_usd: 749 },
+      { country: "Germany", price: 799, currency: "EUR", price_usd: 853 },
+      { country: "Turkey", price: 27999, currency: "TRY", price_usd: 871 },
+    ],
+  },
+  {
+    id: "instapot",
+    name: "Instant Pot Duo 7-in-1",
+    image: "https://m.media-amazon.com/images/I/71bWwYtQZ7L._AC_SL1500_.jpg",
+    prices: [
+      { country: "USA", price: 89, currency: "USD", price_usd: 89 },
+      { country: "Germany", price: 99, currency: "EUR", price_usd: 105 },
+      { country: "Turkey", price: 3499, currency: "TRY", price_usd: 109 },
+    ],
+  },
 ];
 
 export default products;
+// This is a list of products with their prices in different countries.
